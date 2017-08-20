dd888df64ff52aea025b9f54922818b2293f2b4e
Ivelin <nivalen292@outlook.com>
2017-08-20 21:14:52 +0300

    changes to design

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const async = () => {
     return Promise.resolve();
 };
-// 'mongodb://nivalen:123ndb123@ds141232.mlab.com:41232/nivalen-db'
+// use this if u dont have a local database 'mongodb://nivalen:123ndb123@ds141232.mlab.com:41232/nivalen-db'
 async()
-    .then(() => require('./db').init('mongodb://localhost:27017/pizza-db'))
+    .then(() => require('./db').init('mongodb://nivalen:123ndb123@ds141232.mlab.com:41232/nivalen-db'))
     .then((db) => require('./data').init(db))
     .then((data) => require('./app').init(data))
     .then((app) => {
