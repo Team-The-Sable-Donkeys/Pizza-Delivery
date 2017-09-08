c093e1a89bf1dd5ca0dc0604e4273d6c959c63fc
lazarlyutakov <lazarlyutakov@gmail.com>
2017-09-08 13:56:35 +0300

    create pizza validation + toastr mesages

--- src/app/pizzas/finalize-custom-pizza/finalize-custom-pizza.component.ts
+++ src/app/pizzas/finalize-custom-pizza/finalize-custom-pizza.component.ts
@@ -1,8 +1,9 @@
+import { MakeCustomPizzaComponent } from './../make-custom-pizza/make-custom-pizza.component';
 import { PizzaService } from './../../pizza.service';
 import { CustomPizza } from './../make-custom-pizza/custom-pizza.model';
 import { Component, OnInit, Inject, Input, ViewContainerRef } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 import { AuthService } from '../../services/auth/auth.service';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
@@ -21,33 +22,47 @@ export class FinalizeCustomPizzaComponent implements OnInit {
     public customPizza: CustomPizza,
     public auth: AuthService,
     public toastr: ToastsManager,
+    private dialog: MdDialog,
     vcr: ViewContainerRef) {
     this.toastr.setRootViewContainerRef(vcr);
   }
 
+
   addToCart() {
     this.btnDisabled = true;
+    const currPrice = this.customPizza.price;
 
-    this.pizzaService.getUsers()
-      .map((users) => {
-        const loggedUser = users.find((u) => u.authKey === localStorage.getItem('auth-key'));
-        const body = {
-          pizza: this.customPizza,
-          userId: loggedUser.id
-        };
-        this.pizzaService.addToCart(body)
-          .subscribe();
-      })
-      .subscribe(() => {
-        this.btnDisabled = false;
-      });
+    if (!isNaN(currPrice)) {
+      this.pizzaService.getUsers()
+        .map((users) => {
+          const loggedUser = users.find((u) => u.authKey === localStorage.getItem('auth-key'));
+          const body = {
+            pizza: this.customPizza,
+            userId: loggedUser.id
+          };
+          this.pizzaService.addToCart(body)
+            .subscribe();
+        })
+        .subscribe(() => {
+          this.btnDisabled = false;
+        });
 
-    this.toastr.options = {
-      animate: 'flyRight',
-      positionClass: 'toast-top-full-width',
-    };
-    this.toastr.success('Your list has been successfuly cleared!', null, { toastLife: 2000 });
-    this.dialogRef.close();
+      this.toastr.options = {
+        animate: 'flyRight',
+        positionClass: 'toast-top-full-width',
+        showCloseButton: 'true'
+      };
+      this.toastr.success('Your list has been successfuly cleared!', null, { toastLife: 2000 });
+      this.dialogRef.close();
+    } else {
+      this.toastr.options = {
+        animate: 'flyRight',
+        positionClass: 'toast-top-full-width',
+        showCloseButton: 'true'
+      };
+       this.toastr.error('You have to choose pizza size !', null, { toastLife: 4000 });
+      this.toastr.error('You have to choose pizza flour and size!', null, { toastLife: 4000 });
+    }
   }
 
   discard() {
