794c80efc63021ce5ebf9ee3c7b6b85ed348206f
Ivelin <nivalen292@outlook.com>
2017-09-10 20:14:52 +0300

    Fixed every single ts lint error

--- src/app/about/about-contact/about-contact.component.ts
+++ src/app/about/about-contact/about-contact.component.ts
@@ -12,7 +12,9 @@ export class AboutContactComponent implements OnInit {
 
   formIsValid = true;
   errorMessage;
+  /* tslint:disable */
   regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /* tslint:enable */
   name;
   email;
   message;
