bd54fedc885c6ff0d0addef2c17bb3039f917f5f
Ivelin <nivalen292@outlook.com>
2017-09-10 21:42:19 +0300

    fixed pizza service, db urls

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -8,50 +8,51 @@ export class PizzaService {
   constructor(private http: Http) {
   }
 
+  url = 'https://sablepizzahubdata.herokuapp.com/';
+
   getPizzas() {
-    return this.http.get('http://localhost:3000/api/pizzas')
+    return this.http.get(this.url + 'api/pizzas')
       .map((response) => response.json());
   }
 
   getPizzaById(id) {
-    return this.http.get('http://localhost:3000/api/pizzas/' + id)
+    return this.http.get(this.url + 'api/pizzas/' + id)
       .map((response) => response.json());
   }
 
   getUsers() {
-    return this.http.get('http://localhost:3000/api/users')
+    return this.http.get(this.url + 'api/users')
       .map((response) => response.json());
   }
 
   getFixedUser() {
-    return this.http.get('http://localhost:3000/api/users')
+    return this.http.get(this.url + 'api/users')
       .map((response) => response.json().find((u) => u.authKey === localStorage.getItem('auth-key')));
   }
 
   getFixedUserAddress() {
-    return this.http.get('http://localhost:3000/api/users')
+    return this.http.get(this.url + 'api/users')
       .map((response) => response.json().find((u) => u.authKey === localStorage.getItem('auth-key')))
       .map((usr) => usr.address);
   }
 
   getFixedOrder(username) {
-    console.log(username);
-    return this.http.get('http://localhost:3000/api/user-orders')
+    return this.http.get(this.url + 'api/user-orders')
     .map((response) => response.json().filter((o) => o.username === username));
   }
 
   addToCart(body) {
-    return this.http.put('http://localhost:3000/api/cart', body)
+    return this.http.put(this.url + 'api/cart', body)
       .map((response) => response.json());
   }
 
   removeFromCart(body) {
-    return this.http.delete('http://localhost:3000/api/cart', new RequestOptions({
+    return this.http.delete(this.url + 'api/cart', new RequestOptions({
       body: body
     }));
   }
 
   sendOrder(data) {
-    return this.http.post('http://localhost:3000/api/orders', data);
+    return this.http.post(this.url + 'api/orders', data);
   }
 }
