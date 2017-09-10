171c23d347df613106a524cddb0e0ed2cefbcc8a
Ivelin <nivalen292@outlook.com>
2017-09-10 22:36:09 +0300

    fixed login responsive design

--- src/app/pizzas/make-custom-pizza/make-custom-pizza.service.ts
+++ src/app/pizzas/make-custom-pizza/make-custom-pizza.service.ts
@@ -10,28 +10,28 @@ constructor(private http: Http) { }
 url = 'https://sablepizzahubdata.herokuapp.com/';
 
 getSizes() {
-    return this.http.get(this.url + 'custom-pizza-sizes')
+    return this.http.get(this.url + 'api/custom-pizza-sizes')
         .map((res) => res.json());
 }
 
 
 getFlours() {
-    return this.http.get(this.url + 'custom-pizza-flours')
+    return this.http.get(this.url + 'api/custom-pizza-flours')
         .map((res) => res.json());
 }
 
 getMeats() {
-    return this.http.get(this.url + 'custom-pizza-meats')
+    return this.http.get(this.url + 'api/custom-pizza-meats')
         .map((res) => res.json());
 }
 
 getDairies() {
-    return this.http.get(this.url + 'custom-pizza-dairies')
+    return this.http.get(this.url + 'api/custom-pizza-dairies')
         .map((res) => res.json());
 }
 
 getSauces() {
-    return this.http.get(this.url + 'custom-pizza-sauces')
+    return this.http.get(this.url + 'api/custom-pizza-sauces')
         .map((res) => res.json());
 }
 
