83d4f4094d71bbb862c4783b0a6cb892b22af1b5
Kantonov <kk_antonov@abv.bg>
2017-09-10 18:56:32 +0300

    about component test

--- src/app/about/about-contact/about-contact.component.ts
+++ src/app/about/about-contact/about-contact.component.ts
@@ -13,7 +13,7 @@ export class AboutContactComponent implements OnInit {
   formIsValid = true;
   errorMessage;
   regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  constructor(private messagesService: MessagesService,private router: Router) { }
+  constructor(private messagesService: MessagesService, private router: Router) { }
 
   ngOnInit() {
   }
