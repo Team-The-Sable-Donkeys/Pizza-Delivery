f4ba2ca16521691a264e1d54c02bdf788d133560
Ivelin <nivalen292@outlook.com>
2017-09-10 19:49:21 +0300

    refresh page bug fixed

--- src/app/services/orders/orders.service.ts
+++ src/app/services/orders/orders.service.ts
@@ -1,10 +1,11 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { AuthHttp } from 'angular2-jwt';
 
 @Injectable()
 export class OrdersService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private authHttp: AuthHttp) { }
 
   getOrders(page) {
     const params = new URLSearchParams();
